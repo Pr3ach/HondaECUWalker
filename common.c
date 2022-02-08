b190d867d4293f4cc03bb3368c7866d6061ff866
Preacher <Pr3acher@protonmail.com>
2022-02-08 18:18:41 +0100

    fix: open file if it exists, fail otherwise

--- common.c
+++ common.c
@@ -2,7 +2,7 @@
 
 HANDLE open_rom(char* romfile)
 {
-	HANDLE rom = CreateFileA(romfile, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	HANDLE rom = CreateFileA(romfile, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	SetFilePointer(rom, 0, 0, FILE_BEGIN);
 	return rom;
 }
